5e6132956f68b10e036946a40c1bbf351364be1a
Rafael C Santos <rafaelcsan705@gmail.com>
2024-10-11 19:08:42 +0100

    Initial Commit

---
+++ OficinaTimeManager/OficinaTimeManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  OficinaTimeManagerApp.swift
+//  OficinaTimeManager
+//
+//  Created by Rafael Santos on 11/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct OficinaTimeManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OficinaTimeManagerTests/OficinaTimeManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  OficinaTimeManagerTests.swift
+//  OficinaTimeManagerTests
+//
+//  Created by Rafael Santos on 11/10/2024.
+//
+
+import Testing
+@testable import OficinaTimeManager
+
+struct OficinaTimeManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
